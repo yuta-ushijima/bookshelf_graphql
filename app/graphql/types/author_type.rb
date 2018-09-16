bb69161537c393b7d33c77f547842181e5ef4414
yuta-ushijima <register@yuta-u.com>
2018-09-16 23:50:40 +0900

    Update Author Mutation

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,6 +1,8 @@
 class Types::AuthorInputType < GraphQL::Schema::InputObject
   graphql_name "AuthorInputType"
   description "All the attributes for creating an author"
+
+  argument :id, ID, required: false
   argument :first_name, String, required: false, camelize: false
   argument :last_name, String, required: false, camelize: false
   argument :yob, Int, required: false
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,4 +8,14 @@ class Types::MutationType < Types::BaseObject
     Author.create author.to_h
   end
 
+  field :update_author, Boolean, null: false, description: "Update an author" do
+    # updateはフロント側でどんな変更があったかを知る必要がないので、通常ApplicationRecordを返す必要がない。
+    # そのため、Boolean型にしてupdateがtrueなのかfalseなのかを返せばいい。
+    argument :author, Types::AuthorInputType, required: true
+  end
+
+  def update_author(author:)
+    existing = Author.where(id: author[:id]).first
+    existing&.update author.to_h
+  end
 end
